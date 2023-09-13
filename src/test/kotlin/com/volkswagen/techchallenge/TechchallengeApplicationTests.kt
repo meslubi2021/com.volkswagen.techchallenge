66d3fe754fa33333dbeee83b194ec3f4885eaf00
Tomás GARCÍA-POZUELO <tomas.garcia@manomano.com>
2023-09-13 16:37:12 +0200

    Project Initialization from Spring initializr (https://start.spring.io/)

---
+++ src/test/kotlin/com/volkswagen/techchallenge/TechchallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.volkswagen.techchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechchallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
